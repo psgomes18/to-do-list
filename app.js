d5cbdf903d8fe3451bc6d4e1e57fe6830dd147c7
Paulo Sergio <paulogomes1809@gmail.com>
2021-03-15 13:26:53 -0300

    Adding a function to save things on Localstorage

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input')
 const todoButton = document.querySelector('.todo-button')
 const todoList = document.querySelector('.todo-list')
+const filterOption = document.querySelector('.filter-todo')
 
 //Event listener
 todoButton.addEventListener('click', addTodo)
 todoList.addEventListener('click', deleteCheck)
+filterOption.addEventListener('click', filterTodo)
 
 //Functions
 function addTodo(event) {
@@ -36,21 +38,58 @@ function addTodo(event) {
     todoInput.value = ''
 }
 
-function deleteCheck(e){
+function deleteCheck(e) {
     const item = e.target;
     //Delete Todo
-    if(item.classList[0] === 'trash-btn'){
-        const todo = item.ParentElement
+    if (item.classList[0] === 'trash-btn') {
+        const todo = item.parentElement
         //Animation
         todo.classList.add('fall')
-        todo.addEventListener('transitioned', function(){
+        todo.addEventListener('transitioned', function () {
             todo.remove()
         })
     }
 
     //Check Mark
-    if(item.classList[0] === 'complete-btn') {
-        const todo = item.ParentElement
+    if (item.classList[0] === 'complete-btn') {
+        const todo = item.parentElement
         todo.classList.toggle('completed')
     }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes
+    todos.forEach(function (todo) {
+        switch (e.target.value) {
+            case 'all':
+                todo.style.display = 'flex'
+                break
+            case "completed":
+                if (todo.classList.contains('completed')) {
+                    todo.style.display = 'flex'
+                } else {
+                    todo.style.display = 'none'
+                }
+                break
+            case "uncompleted":
+                if (!todo.classList.contains('completed')) {
+                    todo.style.display = 'flex'
+                } else {
+                    todo.style.display = 'none'
+                }
+                break
+        }
+    })
+}
+
+function saveLocalTodos(todo) {
+    //Check
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = []
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'))
+    }
+    todos.push(todo)
+    localStorage.setItem('todos', JSON.stringify(todos))
 }
