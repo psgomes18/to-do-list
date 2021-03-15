67bf6583745ab7d3a22d585e77151fec63842ff4
Paulo Sergio <paulogomes1809@gmail.com>
2021-03-15 13:03:26 -0300

    Adding some new functions to delete items and select list

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector('.todo-list')
 
 //Event listener
 todoButton.addEventListener('click', addTodo)
+todoList.addEventListener('click', deleteCheck)
 
 //Functions
 function addTodo(event) {
@@ -16,7 +17,7 @@ function addTodo(event) {
     todoDiv.classList.add('todo')
     //Create LI
     const newTodo = document.createElement('li')
-    newTodo.innerText = 'hey'
+    newTodo.innerText = todoInput.value
     newTodo.classList.add('todo-item')
     todoDiv.appendChild(newTodo)
     //Check Mark Button
@@ -27,8 +28,29 @@ function addTodo(event) {
     //Check trash button
     const TrashButton = document.createElement('button')
     TrashButton.innerHTML = '<i class="fas fa-trash"></i>'
-    TrashButton.classList.add('complete-btn')
+    TrashButton.classList.add('trash-btn')
     todoDiv.appendChild(TrashButton)
     //Apend to List
-    todoList.appendChild(todoDiv)   
+    todoList.appendChild(todoDiv)
+    //Clear Todo Input Value
+    todoInput.value = ''
+}
+
+function deleteCheck(e){
+    const item = e.target;
+    //Delete Todo
+    if(item.classList[0] === 'trash-btn'){
+        const todo = item.ParentElement
+        //Animation
+        todo.classList.add('fall')
+        todo.addEventListener('transitioned', function(){
+            todo.remove()
+        })
+    }
+
+    //Check Mark
+    if(item.classList[0] === 'complete-btn') {
+        const todo = item.ParentElement
+        todo.classList.toggle('completed')
+    }
 }
